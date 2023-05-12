669c1e6f25121689c2150c41acc8f164df74cf51
Unwana Michael <112607181+godszn@users.noreply.github.com>
2023-05-12 12:57:43 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MainContainer from './components/MainContainer'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import newbtn from './icons/add.png'; 
+
+function App() {
+  return (
+    <div className="App">
+      <MainContainer/>
+    </div>
+  );
+}
+
+export default App;
