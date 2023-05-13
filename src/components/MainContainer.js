f6d98dfeba7aee08e8a51be2c847e6334dc30576
Gift-py <giftabumere247@gmail.com>
2023-05-13 13:58:53 +0100

    beans

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -3,6 +3,7 @@ import './Styles.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import newbtn from '../icons/add.png'; 
 
+
 export class MainContainer extends Component {
   render() {
     return (
@@ -17,9 +18,6 @@ export class MainContainer extends Component {
                     </div>
                 </div>
                 <div className = 'right-nav-widget'>
-                    <div className = 'dummy-img'>
-                      0
-                    </div>
                 </div>
             </div>
         </div>
@@ -54,12 +52,9 @@ export class MainContainer extends Component {
             </div>
             <div className = 'detail-widget d-flex p-4'>
               <p className = 'd-flex p-3'>
-                 <ul className = 'chat-list d-flex p-4 '>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut porttitor,
-                    urna vel maximus consectetur, arcu quam egestas erat, pulvinar blandit lectus nulla eget risus.
-                    Aliquam laoreet, nibh at pulvinar tincidunt, velit sapien malesuada ex, non feugiat ante ante ut elit.
-                    Fusce eu orci ut eros laoreet viverra et vitae elit. Suspendisse potenti. Etiam pretium vel tellus quis rhoncus. Vivamus in lacus vitae arcu pharetra sollicitudin vitae at dui. Fusce iaculis, nulla eu dignissim iaculis, odio sapien imperdiet ante, at pretium quam lacus ac mauris. Integer suscipit felis eu tincidunt feugiat. Proin sed ante id magna feugiat condimentum. Ut aliquam lorem nisi, id semper turpis rutrum sit amet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris vel volutpat nisi.
-                    Aenean sed lectus urna. Integer sed ex magna. Integer egestas lectus ut sem finibus consequat. Duis sodales neque vel pellentesque sodales. Fusce condimentum ex eu urna dapibus, sed dapibus nunc condimentum.
+                 <ul className = 'chat-list d-flex p-4 '><a>
+                    This is going to be the document preview page hhehehe
+                    </a>
                 </ul>
               </p>
             </div>
@@ -67,26 +62,44 @@ export class MainContainer extends Component {
                 <p className = 'd-flex chat-text p-4'>
                 <ul className = 'chat-list d-flex p-4 '>
                     <h5>
-                        Lorem ipsum dolor sit amet
+                        Question 1
                     </h5>
-                    <a>consectetur adipiscing elit. Ut porttitor,
-                    urna vel maximus consectetur, arcu quam egestas erat, pulvinar blandit lectus nulla eget risus.
-                    Aliquam laoreet,</a> <h5>
-                        nibh at pulvinar tincidunt, velit sapien malesuada ex, non feugiat ante ante ut elit.
-                    Fusce eu orci ut eros laoreet</h5> <a>viverra et vitae elit. Suspendisse potenti. Etiam pretium vel tellus quis rhoncus.</a><h5> Vivamus in lacus vitae arcu pharetra sollicitudin vitae at dui. Fusce iaculis, nulla eu dignissim iaculis,</h5> <a> odio sapien imperdiet ante, at pretium quam lacus ac mauris. Integer suscipit felis eu tincidunt feugiat. Proin sed ante id magna feugiat condimentum. Ut aliquam lorem nisi, id semper turpis rutrum sit amet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris vel volutpat nisi.
-                    Aenean sed lectus urna. Integer sed ex magna.</a>
+
+                    <span className = 'span'>Ans:</span>
+
+                    <div id="answer-section" style="display: none;" className = 'Answer-container d-flex p-2'>
+                    <a>Answer 1</a> 
+                    </div>
+                    
                 </ul>
                 </p>
                 <div className = 'generate-wrapper container-fluid d-flex p-4'>
                     <div className = 'btn generate'>
                     <a>Generate questions</a>
                     </div>
-                    <div className = 'btn Answer d-flex'>
+                    <div id="view-answers" onclick="toggleAnswers()" className = 'btn Answer d-flex'>
                     <a>View Answer</a>
                     </div>
                 </div>
             </div>
         </div>
+
+        <script>
+          function toggleAnswers() {
+            let answers =document.getElementsByClassName('Answer-container');
+            let button = document.getElementById("view-answers");
+
+            for (let i = 0; i < answers.length; i++) {
+                if (answers[i].style.display === "none") {
+                  answers[i].style.display = "block";
+                  button.innerHTML = "Hide Answers";
+                } else {
+                  answers[i].style.display = "none";
+                  button.innerHTML = "View Answers";
+                }
+            }
+          }
+        </script>
       </div>
     )
   }
